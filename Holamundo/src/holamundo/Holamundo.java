db089b4d32d868e570b04869e1570061acff4a27
victor <victor@localhost>
2026-07-24 19:32:58 -0600

    Hola mundo

---
+++ Holamundo/src/holamundo/Holamundo.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author victor
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola como estas?");
+        System.out.println("Este es mi primer programa");
+        System.out.println("Yujuuu... Estoy con Java");
+        
+        System.out.printf("HOla estoy %S prpbando printf\n","Victor Reyes" );
+        System.out.printf("%s es buen estudiante","Victor Reyes");
+        System.out.printf("Hola \n como estas \n en la clase %S\n","de programacion?");
+         System.out.printf("Hola \t como estas \t en la clase %S\t","de programacion?");
+         
+         char nombre='V';
+         System.out.printf("Te llamare %c inicial",nombre);
+        
+        // TODO code application logic here
+    }
+    
+}
